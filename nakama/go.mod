d5651e2dda7b7e7aced9e382e257050bc5d8a565
tong <1922527051@qq.com>
2021-09-14 10:19:45 +0800

    init go

---
+++ nakama/go.mod
@@ -0,0 +1,14 @@
+module github.com/tungyao/my_game
+
+go 1.17
+
+require (
+	github.com/heroiclabs/nakama-common v1.17.0
+	github.com/json-iterator/go v1.1.12
+)
+
+require (
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
---
+++ nakama/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"github.com/heroiclabs/nakama-common/runtime"
+	json "github.com/json-iterator/go"
+)
+
+// InitModule All Go modules must have a InitModule function with this exact signature.
+func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
+	// Register the RPC function.
+	if err := initializer.RegisterRpc("my_unique_id", SomeExample); err != nil {
+		logger.Error("Unable to register: %v", err)
+		return err
+	}
+	return nil
+}
+func SomeExample(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+	meta := make(map[string]interface{})
+	// Note below, json.Unmarshal can only take a pointer as second argument
+	if err := json.Unmarshal([]byte(payload), &meta); err != nil {
+		// Handle error
+		return "", err
+	}
+
+	id := "SomeId"
+	authoritative := false
+	sort := "desc"
+	operator := "best"
+	reset := "0 0 * * 1"
+
+	if err := nk.LeaderboardCreate(ctx, id, authoritative, sort, operator, reset, meta); err != nil {
+		// Handle error
+		return "", err
+	}
+
+	return "Success", nil
+}
